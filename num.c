90f6175a83b4d561d604a07c9b80a62ab683242b
Hanane mazyan <hananemzn16@gmail.com>
2023-04-25 18:12:05 -0700

    printf

---
+++ num.c
@@ -0,0 +1,80 @@
+#include "main.h"
+
+/**
+ * convert - a function to convert
+ * @num: number
+ * @flags: flags arguments
+ * @params:  parameters
+ *
+ * Return: string
+ */
+
+char *convert(long int num, int base, int flags, params_t *params)
+{
+	static char *ar; /*array*/
+	static char buf[60];
+	char *p; /* pointer */
+	char s = 0 ; /* sign */
+	unsigned long n = num;
+	(void)params; /* unused variable */
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+	}
+	ar = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF"
+		p = &buf[59];
+	*p = '\0';
+
+	do {
+		*(--p) = ar[n % base];
+		n = n / base;
+	} while (n != 0);
+	if (s)
+		*(--p) = s;
+	return (p);
+}
+
+/**
+ * print_unsigned - print the unsigned integer numbers
+ * @arptr: argument pointer
+ * @params:  parameters
+ *
+ * Return: bytes printed
+ */
+
+int print_unsigned(va_list arptr, params_t *params)
+{
+	unsigned long l;
+
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(arptr, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(arptr, unsigned int);
+	else
+		l = (unsigned int)va_arg(arptr, unsigned int);
+	params->unsign = 1;
+	return (print_number(convert(1, 10, CONVERT_UNSIGNED, params), params));
+}
+/**
+ * print_adress - print the adress
+ * @arptr: argument pointer
+ * @params:  parameters
+ *
+ * Return: bytes printed
+ */
+
+int print_adress(va_list arptr, params_t *params)
+{
+	unsigned long int n = va_arg(arptr, unsigned long int);
+	char *str;
+
+	if (!n)
+		return (_puts("(nil)"));
+
+	str = convert(n, 16, CONVERT_UNSIGNED | CONVERT_LOWCASE, params);
+	*(--str) = 'x';
+	*(--str) = '0';
+	return (print_number(str, params));
+}
